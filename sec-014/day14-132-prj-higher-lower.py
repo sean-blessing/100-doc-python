953ad0c6ce49fd62a0b93b3d602e8ae559657b48
Sean Blessing <snblessing@gmail.com>
2021-01-18 11:39:41 -0500

    finished higher lower game

--- sec-014/day14-132-prj-higher-lower.py
+++ sec-014/day14-132-prj-higher-lower.py
@@ -14,14 +14,31 @@ def get_data_display_string(data):
     str = data["name"] + ", a " + data["description"] + ", from " + data["country"] + "."
     return str
 
+a_data = get_data()
+score = 0
 while not game_over:
-    a_data = get_data()
+    a_followers = a_data["follower_count"]
     b_data = get_data()
+    b_followers = b_data["follower_count"]
 
     print(f"Compare A: {get_data_display_string(a_data)}")
     print(art.vs)
     print(f"Compare B: {get_data_display_string(b_data)}")
-    game_over = True
-
-
-
+    
+    choice = input("\nWho has more followers (A/B)?")
+    if choice == "A":
+        if a_followers > b_followers:
+            score += 1
+            print(f"You're right!  Current score: {score}.")
+        else:
+            game_over = True
+    elif choice == "B":
+        if b_followers > a_followers:
+            score += 1
+            print(f"You're right!  Current score: {score}.")
+            a_data = b_data
+        else:
+            game_over = True
+    else:
+        print("invalid selection")
+print(f"Sorry, that's wrong.  Final score: {score}")
