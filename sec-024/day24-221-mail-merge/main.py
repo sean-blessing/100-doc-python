33722694e9056980de414149fb9f175b93c437b1
Sean Blessing <snblessing@gmail.com>
2021-02-27 17:22:19 -0500

    day 24 mail merge project

--- sec-024/day24-221-mail-merge/main.py
+++ sec-024/day24-221-mail-merge/main.py
@@ -1,15 +1,19 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# open starting_letter and print to console
-f = open("./Input/Letters/starting_letter.txt", "r")
-print(f.readlines())
+names = open("./Input/Names/invited_names.txt", "r")
+names_list = names.readlines()
 
+def build_letter(recipient):
+    template_letter = open("./Input/Letters/starting_letter.txt", "r")
+    template_letter_lines = template_letter.readlines()
+    with open(f"./Output/ReadyToSend/{name}_file.txt", mode="w") as file:
+        for line in template_letter_lines:
+            line = line.replace("\n", "")
+            # for all lines in letter template, replace [name] with name argument and write to new file
+            line = line.replace("[name]",name)
+            file.write(f"{line}\n")
 
+for name in names_list:
+    name = name.replace("\n", "")
+    print(f"generating letter for {name}")
+    build_letter(name)
 
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
